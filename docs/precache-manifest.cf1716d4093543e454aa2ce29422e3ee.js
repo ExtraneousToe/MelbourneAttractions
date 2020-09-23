b50e0830e5afb362c6d7791f417285dc347d4d7c
Matthew Cameron <>
2020-09-23 17:13:05 +1000

    Updated build

---
+++ docs/precache-manifest.cf1716d4093543e454aa2ce29422e3ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c6b7bd803a6fa7fde941881d8cc8cf",
+    "url": "/MelbourneAttractions/index.html"
+  },
+  {
+    "revision": "f2af33de5822813a8f22",
+    "url": "/MelbourneAttractions/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "211e34794a63d9cbfb29",
+    "url": "/MelbourneAttractions/static/css/main.d5059e4a.chunk.css"
+  },
+  {
+    "revision": "f2af33de5822813a8f22",
+    "url": "/MelbourneAttractions/static/js/2.3af69764.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/MelbourneAttractions/static/js/2.3af69764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211e34794a63d9cbfb29",
+    "url": "/MelbourneAttractions/static/js/main.effeeddf.chunk.js"
+  },
+  {
+    "revision": "265fa765e572b11730ff",
+    "url": "/MelbourneAttractions/static/js/runtime-main.d52d40e1.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8fcfd036b4cb1874e3eaff0454200b44.js"
+  "/MelbourneAttractions/precache-manifest.cf1716d4093543e454aa2ce29422e3ee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MelbourneAttractions/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
